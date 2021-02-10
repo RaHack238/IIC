0e60805301870d73ba900604d564118fd3e63f55
khushboogupta13 <guptakhushi345@gmail.com>
2021-02-10 16:01:18 +0530

    added updates

--- src/Pages/Activity/activity.jsx
+++ src/Pages/Activity/activity.jsx
@@ -8,7 +8,18 @@ const Activity = () => {
 
                   </div>
                   <div className="updates">
-
+                        <div style={{display:'flex',flexDirection:'column'}}>
+                              <h1 style={{color:'var(--dark-blue)'}}>Updates</h1>
+                              <div className="individualUpdates">
+                                    <p>Lorem Ipsum</p>
+                                    <br style={{color: 'var(--dark-blue)'}} />
+                                    <p>Lorem Ipsum</p>
+                                    <br style={{color: 'var(--dark-blue)'}} />
+                                    <p>Lorem Ipsum</p>
+                                    <br style={{color: 'var(--dark-blue)'}} />
+                                    <p>Lorem Ipsum</p>
+                              </div>
+                        </div>
                   </div>
             </div>
       )
