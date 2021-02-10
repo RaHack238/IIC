6368af7764a6c97a7925f0618020acc78dd08ea2
Arp!T <arpitchatting@gmail.com>
2021-02-10 15:41:44 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import ContactUs from '../src/Pages/Contact-Us/ContactUs'
+import Activity from "../src/Pages/Activity/activity";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+			<div className='App'>
+				<Route exact to='/' />
+				<Switch>
+					<Route path='/activities' component={Activity} />
+					<Route path='/contact-us' component={ContactUs} />
+				</Switch>
+			</div>
+		);
 }
 
 export default App;
