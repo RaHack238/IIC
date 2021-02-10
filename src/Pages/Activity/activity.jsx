f831f58d3d5b0154175d0aa849de28979d854e2f
Arp!T <arpitchatting@gmail.com>
2021-02-10 17:36:53 +0530

    made ui changes

--- src/Pages/Activity/activity.jsx
+++ src/Pages/Activity/activity.jsx
@@ -16,70 +16,52 @@ const Activity = () => {
 	const imgStyle = {
 		width: '15vw',
 		height: '20vh',
+		cursor: 'pointer'
 	};
 
 	const imgStyle1 = {
 		width: '15vw',
 		height: '18vh',
 		paddingTop: '1.5rem',
+		cursor: 'pointer',
 	};
 
 	const imgStyle2 = {
 		width: '12vw',
 		height: '18vh',
+		cursor: 'pointer',
 	};
 
 	return (
 		<div className='layout'>
 			<div className='activityIcons'>
-				<Table responsive='sm' style={{ padding: '1.5rem' }}>
-					<tr>
-						<td>
-							<img src={funnel} alt='idea-funnel' style={imgStyle} />
-						</td>
-						<td>
-							<img src={ignite} alt='ignite' style={imgStyle1} />
-						</td>
-						<td>
-							<img src={acdprogram} alt='acdprogram' style={imgStyle1} />
-						</td>
-						<td>
-							<img src={startuplab} alt='startup-lab' style={imgStyle} />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<img src={incubation} alt='incubation' style={imgStyle} />
-						</td>
-						<td>
-							<img src={workshops} alt='workshops' style={imgStyle} />
-						</td>
-						<td>
-							<img src={talks} alt='talks' style={imgStyle} />
-						</td>
-						<td>
-							<img src={mic} alt='mic-activity' style={imgStyle} />
-						</td>
-					</tr>
-					<tr>
-						<td style={{ textAlign: 'center' }}>
-							<img src={visits} alt='field-visits' style={imgStyle2} />
-						</td>
-						<td style={{ textAlign: 'center' }}>
-							<img src={upcevents} alt='upc-events' style={imgStyle2} />
-						</td>
-					</tr>
-				</Table>
+				<div
+					style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+				>
+					<img src={funnel} alt='idea-funnel' style={imgStyle} />
+					<img src={ignite} alt='ignite' style={imgStyle1} />
+					<img src={acdprogram} alt='acdprogram' style={imgStyle1} />
+					<img src={startuplab} alt='startup-lab' style={imgStyle} />
+				</div>
+				<div
+					style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+				>
+					<img src={incubation} alt='incubation' style={imgStyle} />
+					<img src={workshops} alt='workshops' style={imgStyle} />
+					<img src={talks} alt='talks' style={imgStyle} />
+					<img src={mic} alt='mic-activity' style={imgStyle} />
+				</div>
+				<div
+					style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+				>
+					<img src={visits} alt='field-visits' style={imgStyle1} />
+					<img src={upcevents} alt='upc-events' style={imgStyle1} />
+				</div>
 			</div>
 			<div className='container updates'>
 				<div style={{ display: 'flex', flexDirection: 'column' }}>
 					<h1 style={{ color: 'var(--dark-blue)', marginTop: '4rem' }}>Updates</h1>
 					<div className='individualUpdates'>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua
-							<hr />
-						</p>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 							tempor incididunt ut labore et dolore magna aliqua
