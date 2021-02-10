ee68f0c235af0f9a83647b4ece7c7aa993349b7f
khushboogupta13 <guptakhushi345@gmail.com>
2021-02-10 15:37:04 +0530

    added activity component

---
+++ src/Pages/Activity/activity.jsx
@@ -0,0 +1,17 @@
+import {React} from 'react';
+import './activity.css';
+
+const Activity = () => {
+      return(
+            <div className="layout">
+                  <div className="activityIcons">
+
+                  </div>
+                  <div className="updates">
+
+                  </div>
+            </div>
+      )
+}
+
+export default Activity;
