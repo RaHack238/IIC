911b0cb339c947dd674f7268a3ca1a08a5c1cddc
Arp!T <arpitchatting@gmail.com>
2021-02-10 21:54:25 +0530

    made ui changes

--- src/Pages/Activity/activity.jsx
+++ src/Pages/Activity/activity.jsx
@@ -1,83 +1,63 @@
-import { React } from 'react';
-import { Table } from 'react-bootstrap';
+import { React, useState } from 'react';
 import './activity.css';
-import funnel from './ideaFunnel.svg';
-import ignite from './ignite.svg';
-import acdprogram from './academicProgram.svg';
-import startuplab from './StartupLab.svg';
-import visits from './FieldVisit.svg';
-import upcevents from './UpcomingEvents.svg';
-import incubation from './Incubation.svg';
-import mic from './MICActivity.svg';
-import talks from './Talks.svg';
-import workshops from './Workshops.svg';
+import ActivityIcons from "../../Components/activityIcons/activityIcons";
 
 const Activity = () => {
-	const imgStyle = {
-		width: '15vw',
-		height: '20vh',
-		cursor: 'pointer'
-	};
-
-	const imgStyle1 = {
-		width: '15vw',
-		height: '18vh',
-		paddingTop: '1.5rem',
-		cursor: 'pointer',
-	};
-
-	const imgStyle2 = {
-		width: '12vw',
-		height: '18vh',
-		cursor: 'pointer',
-	};
-
+	
+	const [view, setView] = useState({
+		ifView: false,
+		igniteView: false,
+		aepView: false,
+		LabView: false,
+		incubationView: false,
+		workshopView: false,
+		eniView: false,
+		micView: false,
+		fieldVisits: false,
+		upcevents: false,
+	});
 	return (
-		<div className='layout'>
-			<div className='activityIcons'>
-				<div className='iconDiv'>
-					<img src={funnel} alt='idea-funnel' style={imgStyle} />
-					<img src={ignite} alt='ignite' style={imgStyle1} />
-					<img src={acdprogram} alt='acdprogram' style={imgStyle1} />
-					<img src={startuplab} alt='startup-lab' style={imgStyle} />
+		<div>
+			<div className='layout'>
+				<div className='activityIcons'>
+					<ActivityIcons setView={setView} />
 				</div>
-				<div className='iconDiv'>
-					<img src={incubation} alt='incubation' style={imgStyle} />
-					<img src={workshops} alt='workshops' style={imgStyle} />
-					<img src={talks} alt='talks' style={imgStyle} />
-					<img src={mic} alt='mic-activity' style={imgStyle} />
-				</div>
-				<div className='iconDiv'>
-					<img src={visits} alt='field-visits' style={imgStyle1} />
-					<img src={upcevents} alt='upc-events' style={imgStyle1} />
-				</div>
-			</div>
-			<div className='container updates'>
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<h1 style={{ color: 'var(--dark-blue)', marginTop: '4rem' }}>Updates</h1>
-					<div className='individualUpdates'>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua
-							<hr />
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua
-							<hr />
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua
-							<hr />
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua
-						</p>
+				<div className='container updates'>
+					<div style={{ display: 'flex', flexDirection: 'column' }}>
+						<h1 style={{ color: 'var(--dark-blue)', marginTop: '4rem' }}>Updates</h1>
+						<div className='individualUpdates'>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua
+								<hr />
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua
+								<hr />
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua
+								<hr />
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua
+								<hr />
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
+			<div
+				className='individualComponents'
+				style={{ display: view.ifView ? 'block' : 'none' }}
+			></div>
 		</div>
 	);
 };
