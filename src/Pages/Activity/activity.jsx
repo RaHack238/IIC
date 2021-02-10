481fb0382009c567cbe9eaeb5501d15a8ccf2175
Arp!T <arpitchatting@gmail.com>
2021-02-10 16:35:50 +0530

    added update section

--- src/Pages/Activity/activity.jsx
+++ src/Pages/Activity/activity.jsx
@@ -1,28 +1,38 @@
-import {React} from 'react';
+import { React } from 'react';
 import './activity.css';
 
 const Activity = () => {
-      return(
-            <div className="layout">
-                  <div className="activityIcons">
-
-                  </div>
-                  <div className="updates">
-                        <div style={{display:'flex',flexDirection:'column'}}>
-                              <h1 style={{color:'var(--dark-blue)'}}>Updates</h1>
-                              <div className="individualUpdates">
-                                    <p>Lorem Ipsum</p>
-                                    <br style={{color: 'var(--dark-blue)'}} />
-                                    <p>Lorem Ipsum</p>
-                                    <br style={{color: 'var(--dark-blue)'}} />
-                                    <p>Lorem Ipsum</p>
-                                    <br style={{color: 'var(--dark-blue)'}} />
-                                    <p>Lorem Ipsum</p>
-                              </div>
-                        </div>
-                  </div>
-            </div>
-      )
-}
+	return (
+		<div className='layout'>
+			<div className='activityIcons'></div>
+			<div className='container updates'>
+				<div style={{ display: 'flex', flexDirection: 'column' }}>
+					<h1 style={{ color: 'var(--dark-blue)', marginTop: '4rem' }}>Updates</h1>
+					<div className='individualUpdates'>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua
+						</p>
+						<hr />
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua
+						</p>
+						<hr />
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua
+						</p>
+						<hr />
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Activity;
