c3d7f2073738b7f0cc85934b7ef0d3603ebd5212
Sahil Verma <sahilverma817@gmail.com>
2021-02-21 13:33:09 +0530

    homepage(reponsive) and navbar(unreponsive)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import ContactUs from '../src/Pages/Contact-Us/ContactUs';
 import Activity from '../src/Pages/Activity/activity';
+import Home from './Components/Home/Home'
 
 function App() {
 	return (
 		<div className='App'>
+			<Home />
 			<Route exact to='/' />
 			<Switch>
 				<Route path='/activities' component={Activity} />
