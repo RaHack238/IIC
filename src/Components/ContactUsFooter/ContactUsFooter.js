b20528943352018611d4bd8609494e0208c888bb
Arpit Maurya <arpitchatting@gmail.com>
2021-07-20 14:33:43 +0530

    fix footer toggle

--- src/Components/ContactUsFooter/ContactUsFooter.js
+++ src/Components/ContactUsFooter/ContactUsFooter.js
@@ -1,168 +1,124 @@
-import React, { Component } from "react";
-import "./ContactUsFooter.css";
-import down_arrow from "./assets/down_arrow.svg";
-import up_arrow from "./assets/up_arrow.svg";
-import twitter_icon from "./assets/twitter.svg";
-import email_icon from "./assets/email.svg";
+import React, { Component } from 'react';
+import './ContactUsFooter.css';
+import down_arrow from './assets/down_arrow.svg';
+import up_arrow from './assets/up_arrow.svg';
+import twitter_icon from './assets/twitter.svg';
+import email_icon from './assets/email.svg';
 
 export default class ContactUsFooter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { windowWidth: window.innerWidth, footer_open: false };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { windowWidth: window.innerWidth, footer_open: false };
+		console.log(this.state.footer_open);
+	}
 
-  handleResize = (e) => {
-    this.setState({ windowWidth: window.innerWidth });
-  };
+	handleResize = (e) => {
+		this.setState({ windowWidth: window.innerWidth });
+	};
 
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-  }
+	componentDidMount() {
+		window.addEventListener('resize', this.handleResize);
+		setTimeout(() => this.setState({ footer_open: this.props.footer }), 1);
+	}
 
-  componentWillUnmount() {
-    window.addEventListener("resize", this.handleResize);
-  }
-  toggle_footer = () => {
-    this.setState({ footer_open: !this.state.footer_open });
-    console.log(this.state.windowWidth);
-  };
-  render() {
-    return (
-      <div
-        className={
-          "contact__us__footer " +
-          (this.state.footer_open ? "footer__open" : "footer__closed")
-        }
-      >
-        <div className="footer__arrow__div" onClick={this.toggle_footer}>
-          <img
-            src={this.state.footer_open ? down_arrow : up_arrow}
-            className="arrow__img"
-          />
-        </div>
-        {this.state.windowWidth > 578 ? (
-          <div className="row contact__us_info__div">
-            <div className="footer__left col-sm-6 col-xs-12">
-              <div className="icons__div">
-                <a href="https://twitter.com/iiciitgoa">
-                  <img
-                    src={twitter_icon}
-                    className="footer__icon"
-                    alt="twitter"
-                  />
-                </a>
-                <a href="mailto:iic@iitgoa.ac.in">
-                  <img src={email_icon} className="footer__icon" alt="email" />
-                </a>
-              </div>
-              <p className="left__column__text">
-                <p className="contact__us__mobile__text">
-                  <a
-                    className="contact__us__mobile__text_1"
-                    href="https://mic.gov.in/"
-                  >
-                    MIC
-                  </a>
-                </p>
-                <p className="contact__us__mobile__text">
-                  <a
-                    className="contact__us__mobile__text_1"
-                    href="https://iitgoa.ac.in/aboutiit.php?var=newsletter"
-                  >
-                    Gurukal Varta
-                  </a>
-                </p>
-                <p className="contact__us__mobile__text">
-                  <a
-                    className="contact__us__mobile__text_1"
-                    href="https://www.aicte-india.org/"
-                  >
-                    AICTE
-                  </a>
-                </p>
-              </p>
-            </div>
-            <div className="footer__right col-sm-6 col-xs-12">
-              <p className="right__column__text">
-                Indian Institute of Technology
-              </p>
-              <p className="right__column__text">Goa</p>
-              <p className="right__column__text">GEC Campus</p>
-              <p className="right__column__text">Veling</p>
-              <p className="right__column__text">Ponda, Goa 403401</p>
-            </div>
-          </div>
-        ) : (
-          <div className="footer__info__div__mobile">
-            <div className="icons__div_mobile">
-              <a href="https://twitter.com/iiciitgoa">
-                <img
-                  src={twitter_icon}
-                  className="footer__icon_mobile"
-                  alt="twitter"
-                />
-              </a>
-              <a href="mailto:iic@iitgoa.ac.in">
-                <img
-                  src={email_icon}
-                  className="footer__icon_mobile"
-                  alt="email"
-                />
-              </a>
-            </div>
-            <p className="contact__us__mobile__text">
-              <a
-                className="contact__us__mobile__text_1"
-                href="https://mic.gov.in/"
-              >
-                MIC
-              </a>
+	componentWillUnmount() {
+		window.addEventListener('resize', this.handleResize);
+	}
+	toggle_footer = () => {
+		this.setState({ footer_open: !this.state.footer_open });
+		console.log(this.state.windowWidth);
+	};
+	render() {
+		return (
+			<div className={'contact__us__footer ' + (this.state.footer_open ? 'footer__open' : 'footer__closed')}>
+				<div className='footer__arrow__div' onClick={this.toggle_footer}>
+					<img src={this.state.footer_open ? down_arrow : up_arrow} className='arrow__img' />
+				</div>
+				{this.state.windowWidth > 578 ? (
+					<div className='row contact__us_info__div'>
+						<div className='footer__left col-sm-6 col-xs-12'>
+							<div className='icons__div'>
+								<a href='https://twitter.com/iiciitgoa'>
+									<img src={twitter_icon} className='footer__icon' alt='twitter' />
+								</a>
+								<a href='mailto:iic@iitgoa.ac.in'>
+									<img src={email_icon} className='footer__icon' alt='email' />
+								</a>
+							</div>
+							<p className='left__column__text'>
+								<p className='contact__us__mobile__text'>
+									<a className='contact__us__mobile__text_1' href='https://mic.gov.in/'>
+										MIC
+									</a>
+								</p>
+								<p className='contact__us__mobile__text'>
+									<a
+										className='contact__us__mobile__text_1'
+										href='https://iitgoa.ac.in/aboutiit.php?var=newsletter'>
+										Gurukal Varta
+									</a>
+								</p>
+								<p className='contact__us__mobile__text'>
+									<a className='contact__us__mobile__text_1' href='https://www.aicte-india.org/'>
+										AICTE
+									</a>
+								</p>
+							</p>
+						</div>
+						<div className='footer__right col-sm-6 col-xs-12'>
+							<p className='right__column__text'>Indian Institute of Technology</p>
+							<p className='right__column__text'>Goa</p>
+							<p className='right__column__text'>GEC Campus</p>
+							<p className='right__column__text'>Veling</p>
+							<p className='right__column__text'>Ponda, Goa 403401</p>
+						</div>
+					</div>
+				) : (
+					<div className='footer__info__div__mobile'>
+						<div className='icons__div_mobile'>
+							<a href='https://twitter.com/iiciitgoa'>
+								<img src={twitter_icon} className='footer__icon_mobile' alt='twitter' />
+							</a>
+							<a href='mailto:iic@iitgoa.ac.in'>
+								<img src={email_icon} className='footer__icon_mobile' alt='email' />
+							</a>
+						</div>
+						<p className='contact__us__mobile__text'>
+							<a className='contact__us__mobile__text_1' href='https://mic.gov.in/'>
+								MIC
+							</a>
 
-              <a
-                className="contact__us__mobile__text_1"
-                href="https://iitgoa.ac.in/aboutiit.php?var=newsletter"
-              >
-                Gurukal Varta
-              </a>
-              <a
-                className="contact__us__mobile__text_1"
-                href="https://www.aicte-india.org/"
-              >
-                AICTE
-              </a>
-            </p>
-            <div className="iit__goa__text">
-              <p className="right__column__text__mobile">
-                Indian Institute of Technology Goa
-              </p>
-              <p className="right__column__text__mobile">GEC Campus</p>
-              <p className="right__column__text__mobile">Veling</p>
-              <p className="right__column__text__mobile">Ponda, Goa 403401</p>
-            </div>
-          </div>
-        )}
-        {this.state.windowWidth > 578 ? (
-          <div className="footer__copyright__div">
-            <p className="footer__copyright__text">Copyright: IIT GOA</p>
-            <p className="footer__copyright__text">Partners: Wadhwani</p>
-            <p className="footer__copyright__text">
-              Designed by: ArchiTechs2021
-            </p>
-          </div>
-        ) : (
-          <div className="footer__copyright__div_mobile">
-            <p className="footer__copyright__text__mobile">
-              Copyright: IIT GOA
-            </p>
-            <p className="footer__copyright__text__mobile">
-              Partners: Wadhwani
-            </p>
-            <p className="footer__copyright__text__mobile">
-              Designed by: ArchiTechs2021
-            </p>
-          </div>
-        )}
-      </div>
-    );
-  }
+							<a
+								className='contact__us__mobile__text_1'
+								href='https://iitgoa.ac.in/aboutiit.php?var=newsletter'>
+								Gurukal Varta
+							</a>
+							<a className='contact__us__mobile__text_1' href='https://www.aicte-india.org/'>
+								AICTE
+							</a>
+						</p>
+						<div className='iit__goa__text'>
+							<p className='right__column__text__mobile'>Indian Institute of Technology Goa</p>
+							<p className='right__column__text__mobile'>GEC Campus</p>
+							<p className='right__column__text__mobile'>Veling</p>
+							<p className='right__column__text__mobile'>Ponda, Goa 403401</p>
+						</div>
+					</div>
+				)}
+				{this.state.windowWidth > 578 ? (
+					<div className='footer__copyright__div'>
+						<p className='footer__copyright__text'>Copyright: IIT GOA</p>
+						<p className='footer__copyright__text'>Partners: Wadhwani</p>
+						<p className='footer__copyright__text'>Designed by: ArchiTechs2021</p>
+					</div>
+				) : (
+					<div className='footer__copyright__div_mobile'>
+						<p className='footer__copyright__text__mobile'>Copyright: IIT GOA</p>
+						<p className='footer__copyright__text__mobile'>Partners: Wadhwani</p>
+						<p className='footer__copyright__text__mobile'>Designed by: ArchiTechs2021</p>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
--- src/Pages/Contact-Us/ContactUs.js
+++ src/Pages/Contact-Us/ContactUs.js
@@ -1,58 +1,62 @@
-import React, { Component } from "react";
-import ContactUsFooter from "../../Components/ContactUsFooter/ContactUsFooter";
-import "./ContactUs.css";
-import iiclogo from "./assets/iiclogo.svg";
-import iitlogo from "./assets/iit goa logo@1X.svg";
+import React, { Component } from 'react';
+import ContactUsFooter from '../../Components/ContactUsFooter/ContactUsFooter';
+import './ContactUs.css';
+import iiclogo from './assets/iiclogo.svg';
+import iitlogo from './assets/iit goa logo@1X.svg';
 
-import PersonCard from "../../Components/PersonCard/PersonCard";
-import people from "./assets/people.js";
-import NavBar from "../../Components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import PersonCard from '../../Components/PersonCard/PersonCard';
+import people from './assets/people.js';
+import NavBar from '../../Components/Navbar/Navbar';
+import { Link, useLocation } from 'react-router-dom';
 
-export default class ContactUs extends Component {
-  state = { footer_visible: false };
+const ContactUs = () => {
+	// state = { footer_visible: false };
+	const location = useLocation();
+	const footer = !location.state ? false : true;
+	console.log(footer);
+	// componentDidMount() {
+	//   console.log(people);
+	// }
+	// render() {
+	return (
+		<div className='contact__us'>
+			<div className='navbar__div'>
+				<div className='navbar__left'>
+					<a href='https://iitgoa.ac.in/' target='_blank' rel='noreferrer'>
+						<img src={iitlogo} alt='IIT Goa' className='iit__goa__logo' />
+					</a>
+					<Link to='/'>
+						<img
+							src={iiclogo}
+							alt='IIC'
+							style={{ width: '10vw', marginTop: '0.6vw' }}
+							className='iic__logo'
+						/>
+					</Link>
+				</div>
+				<div className='navbar__center'>
+					<h1 className='navbar__title'>Council</h1>
+				</div>
+				<div className='navbar__right'></div>
+			</div>
+			<NavBar />
+			<div className='row main__council__window'>
+				{people.map((person, index) => {
+					return (
+						<PersonCard
+							name={person.name}
+							designation={person.designation}
+							index={2 * index}
+							key={index}
+							img={person.photo}
+						/>
+					);
+				})}
+			</div>
+			<ContactUsFooter footer={footer} />
+		</div>
+	);
+	// }
+};
 
-  componentDidMount() {
-    console.log(people);
-  }
-  render() {
-    return (
-      <div className="contact__us">
-        <div className="navbar__div">
-          <div className="navbar__left">
-            <a href="https://iitgoa.ac.in/" target="_blank" rel="noreferrer">
-              <img src={iitlogo} alt="IIT Goa" className="iit__goa__logo" />
-            </a>
-            <Link to="/">
-              <img
-                src={iiclogo}
-                alt="IIC"
-                style={{ width: "10vw", marginTop: "0.6vw" }}
-                className="iic__logo"
-              />
-            </Link>
-          </div>
-          <div className="navbar__center">
-            <h1 className="navbar__title">Council</h1>
-          </div>
-          <div className="navbar__right"></div>
-        </div>
-        <NavBar />
-        <div className="row main__council__window">
-          {people.map((person, index) => {
-            return (
-              <PersonCard
-                name={person.name}
-                designation={person.designation}
-                index={2 * index}
-                key={index}
-                img={person.photo}
-              />
-            );
-          })}
-        </div>
-        <ContactUsFooter />
-      </div>
-    );
-  }
-}
+export default ContactUs;
